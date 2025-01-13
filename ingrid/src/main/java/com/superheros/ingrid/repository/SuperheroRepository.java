ba966f491ab2ec8b3eb38eca8f0e6b25b2518f73
Elielo03 <elielorgz03@gmail.com>
2025-01-13 01:58:40 -0600

    Update SuperheroRepository.java

--- ingrid/src/main/java/com/superheros/ingrid/repository/SuperheroRepository.java
+++ ingrid/src/main/java/com/superheros/ingrid/repository/SuperheroRepository.java
@@ -1,5 +1,6 @@
 package com.superheros.ingrid.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
